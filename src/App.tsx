7988d45ce2376ccf7de9005bba31f3aa562466e9
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-12-21 22:31:03 -0800

    install radix-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { Flex, Text, Button } from "@radix-ui/themes";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,10 @@ function App() {
       <h1 className="text-3xl font-bold underline text-center bg-green-300">
         Hello world Srikanth!
       </h1>
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes :)</Text>
+        <Button>Let's go</Button>
+      </Flex>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@radix-ui/themes/styles.css";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Theme
+      appearance="light"
+      accentColor="grass"
+      grayColor="gray"
+      radius="large"
+    >
+      <App />
+      {/* <ThemePanel /> */}
+    </Theme>
+  </React.StrictMode>
+);
