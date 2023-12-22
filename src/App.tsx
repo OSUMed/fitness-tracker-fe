e90af575d855d6f6a3b516043967c2e27c77cdea
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-12-21 22:25:56 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center bg-green-300">
+        Hello world Srikanth!
+      </h1>
+    </>
+  );
+}
+
+export default App;
