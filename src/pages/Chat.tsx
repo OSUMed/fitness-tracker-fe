4d3b105b0e62fc8629f68745796d9e8704508719
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-12-24 23:31:56 -0800

    add working logout functionality.
    pass jwt from local storage and clear local storage if success request.
    reset context username so login page gets rendered in routes

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,16 +1,42 @@
 import React, { useContext } from "react";
 import { UserContext } from "../context/UserContext";
+import axios from "axios";
 
 const Chat: React.FC = () => {
   const context = useContext(UserContext);
 
   // Assert that context is not undefined
-  const { username } = context!;
+  const { username, setUsername: setContextUsername } = context!;
+
+  const handleLogout = async () => {
+    try {
+      const jwtToken = localStorage.getItem("accessToken");
+
+      if (jwtToken) {
+        await axios.post(
+          "http://localhost:8080/api/logout",
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${jwtToken}`,
+            },
+            withCredentials: true,
+          }
+        );
+      }
+
+      localStorage.clear();
+      setContextUsername(null);
+    } catch (error) {
+      console.error("Logout failed", error);
+    }
+  };
 
   return (
     <div>
       <h1>Chat</h1>
       <p>Welcome, {username}</p>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
