6104a3a675686de8b73e8c338cfba2f97d47fd16
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-12-30 06:25:40 -0800

    add custom colors to CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#607D8B", // Primary Color
+        secondary: {
+          green: "#4CAF50", // Pastel Green
+          gray: "#E0E0E0", // Gentle Gray
+        },
+        accent: "#FFEB3B", // Sunshine Yellow
+        charcoal: "#333333", // Charcoal Black
+      },
+    },
   },
   plugins: [],
 };
